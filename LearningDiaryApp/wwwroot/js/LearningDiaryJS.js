a9f8adedc4e5e48bdc014cec6a9d835898e82a57
laurapesonen <laura-pesonen@hotmail.com>
2022-07-29 15:07:16 +0300

    vko 7 commit

---
+++ LearningDiaryApp/wwwroot/js/LearningDiaryJS.js
@@ -0,0 +1,11 @@
+
+//Jeren funktio
+/*var poisto = document.getElementById("delete");
+poisto.addEventListener('submit', (event) => {
+    event.preventDefault();
+    var choice = confirm("Delete");
+    if (choice)
+    {
+        poisto.submit();
+    }
+});*/
